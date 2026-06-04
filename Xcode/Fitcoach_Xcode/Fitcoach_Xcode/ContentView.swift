9a0c618b009a796300f535df76ec93d6c0cf1e66
Black <beardedblackmallu@users.noreply.github.com>
2026-06-04 14:34:53 +0530

    CP5: iOS CocoaPods fixed + LANG=en_US.UTF-8 workaround in npm run ios
    ios/App/Podfile.lock: 12 pods installed (Capacitor 8 + plugins)
    package.json: ios script now sets LANG=en_US.UTF-8 before cap sync
    to work around CocoaPods 1.16.2 + Ruby 4.0.5 encoding incompatibility

---
+++ Xcode/Fitcoach_Xcode/Fitcoach_Xcode/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fitcoach_Xcode
+//
+//  Created by Vishnu Nair on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ Xcode/Fitcoach_Xcode/Fitcoach_Xcode/Fitcoach_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitcoach_XcodeApp.swift
+//  Fitcoach_Xcode
+//
+//  Created by Vishnu Nair on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Fitcoach_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
